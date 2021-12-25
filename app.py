3b54e0b204d11ed1958dee9b3b2ca938fe4ca3ed
Pranoy Dutta <pranoydll@gmail.com>
2021-12-25 15:01:51 -0600

    Added login and start of python

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, session
+from flask_login import LoginManager
+import sqlite3
+
+app = Flask(__name__)
+#TODO make an ENV var
+app.secret_key = b'5cc3b1d033e1c56c84376684a9a6122de864f1cd5da93f540e127e0a0caf2e1e'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+con = sqlite3.connect('re')
+
+@login_manager.user_loader
+def load_user(user_id):
