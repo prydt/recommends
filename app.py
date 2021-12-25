0c6061574e6495a5ed6edc73782403b463f7bc7d
Pranoy Dutta <pranoydll@gmail.com>
2021-12-25 15:38:54 -0600

    Added index page

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ app.secret_key = b'5cc3b1d033e1c56c84376684a9a6122de864f1cd5da93f540e127e0a0caf2
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-con = sqlite3.connect('re')
+con = sqlite3.connect('recommends.db')
+cur = sqlite.cursor()
 
 @login_manager.user_loader
 def load_user(user_id):
+    cur.execute('SELECT * FROM users WHERE id = ?', user_id)
+    user = cur.fetchall()
+    # TODO
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
